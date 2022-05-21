6475d1a4e0230f2a0b070442e3c89fef2dec3a56
mesnardo <mesnardo@gwu.edu>
2022-05-21 13:57:42 -0400

    Refine figure surface cp both-Re

--- runs/snake3d/both_lips/scripts/plot_surface_pressure_coefficient_35.py
+++ runs/snake3d/both_lips/scripts/plot_surface_pressure_coefficient_35.py
@@ -15,11 +15,11 @@ args = rodney.parse_command_line(is_slow=True)
 maindir = pathlib.Path(__file__).absolute().parents[1]
 figdir = maindir / 'figures'
 
-folders = ['1k35', '2k35', '3k35']
+cases = {'Re=1000': '1k35', 'Re=2000': '2k35', 'Re=3000': '3k35'}
 
 cp_objs = [
-    rodney.SurfacePressureData(folder, maindir / folder)
-    for folder in folders
+    rodney.SurfacePressureData(label, maindir / folder)
+    for label, folder in cases.items()
 ]
 
 times = numpy.round(
@@ -35,17 +35,16 @@ for cp_obj in cp_objs:
         cp_obj.load('surface_pressure_coefficient_100_200.txt')
 
 # Set default font family and size for Matplotlib figures.
-pyplot.rc('font', family='serif', size=14)
+pyplot.rc('font', family='serif', size=12)
 
 # Plot the surface pressure coefficient.
-fig, ax = pyplot.subplots(figsize=(8.0, 4.0))
+fig, ax = pyplot.subplots(figsize=(6.0, 4.0))
 ax.set_xlabel('$x / c$')
 ax.set_ylabel('$C_p$')
+wrap = lambda arr: numpy.append(arr, arr[0])
 for cp_obj in cp_objs:
-    ax.plot(cp_obj.x, cp_obj.values, label=cp_obj.label)
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 1.0, box.height])
-ax.legend(loc='upper left', bbox_to_anchor=(1, 1), frameon=False)
+    ax.plot(wrap(cp_obj.x), wrap(cp_obj.values), label=cp_obj.label)
+ax.legend(loc='lower right', frameon=False, fontsize=12)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 fig.tight_layout()
