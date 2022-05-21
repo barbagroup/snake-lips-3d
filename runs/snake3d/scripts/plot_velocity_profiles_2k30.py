67eb3ce7fb021f788c27a6ce1525e1d59076212b
mesnardo <mesnardo@gwu.edu>
2022-05-21 11:57:36 -0400

    Refine figure velocity profiles 2k30

--- runs/snake3d/scripts/plot_velocity_profiles_2k30.py
+++ runs/snake3d/scripts/plot_velocity_profiles_2k30.py
@@ -44,30 +44,37 @@ for lip_cfg in cases.keys():
             data[f'{lip_cfg}_{Re}{AoA}'] = vel_obj
 
 # Set default font family and size of Matplotlib figures.
-pyplot.rc('font', family='serif', size=14)
+pyplot.rc('font', family='serif', size=12)
 
 # Plot vertical profiles of the mean streamwise velocity.
-fig, ax = pyplot.subplots(figsize=(8.0, 4.0))
+fig, ax = pyplot.subplots(figsize=(6.0, 4.0))
 ax.text(0.01, 0.9, r'$<u> / U_\infty - 1$', transform=ax.transAxes)
-ax.set_xlabel('x / c')
-ax.set_ylabel('y / c')
+ax.set_xlabel('$x / c$')
+ax.set_ylabel('$y / c$')
 U_inf, c = 1.0, 1.0
 xlocs = [1.06, 1.54, 2.02]
+
+
+def relabel(label):
+    new_labels = ['both', 'back', 'front', 'none']
+    for elem in new_labels:
+        if elem in label:
+            return elem
+    return 'none'
+
+
 for i, (label, vel_obj) in enumerate(data.items()):
     for iloc, xloc in enumerate(xlocs):
-        plt_label = label if iloc == 0 else None
+        ax.axvline(iloc, color='black', linewidth=0.5)
+        plt_label = relabel(label) if iloc == 0 else None
         y = vel_obj.y
         ux = vel_obj.values[xloc]['ux']
         ax.plot(iloc + (ux - U_inf) / U_inf, y / c,
                 label=plt_label, color=f'C{i}')
-        ax.axvline(iloc, color='black', linestyle=':', linewidth=0.5)
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 1.0, box.height])
-ax.legend(loc='upper left', bbox_to_anchor=(1, 1),
-          frameon=False, prop=dict(size=12))
+ax.legend(loc='lower left', frameon=False, fontsize=12)
 ax.set_xticks(range(len(xlocs)))
 ax.set_xticklabels(xlocs)
-ax.set_ylim(-3.0, 3.0)
+ax.axis([-1.0, 2.1, -3.0, 3.0])
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 fig.tight_layout()
@@ -78,26 +85,23 @@ if args.save_figures:
     fig.savefig(filepath, dpi=300, bbox_inches='tight')
 
 # Plot vertical profiles of the mean transversal velocity.
-fig, ax = pyplot.subplots(figsize=(8.0, 4.0))
+fig, ax = pyplot.subplots(figsize=(6.0, 4.0))
 ax.text(0.01, 0.9, r'$<v> / U_\infty$', transform=ax.transAxes)
-ax.set_xlabel('x / c')
-ax.set_ylabel('y / c')
+ax.set_xlabel('$x / c$')
+ax.set_ylabel('$y / c$')
 xlocs = [1.06, 1.54, 2.02]
 for i, (label, vel_obj) in enumerate(data.items()):
     for iloc, xloc in enumerate(xlocs):
-        plt_label = label if iloc == 0 else None
+        ax.axvline(iloc, color='black', linewidth=0.5)
+        plt_label = relabel(label) if iloc == 0 else None
         y = vel_obj.y
         uy = vel_obj.values[xloc]['uy']
         ax.plot(iloc + uy / U_inf, y / c,
                 label=plt_label, color=f'C{i}')
-        ax.axvline(iloc, color='black', linestyle=':', linewidth=0.5)
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 1.0, box.height])
-ax.legend(loc='upper left', bbox_to_anchor=(1, 1),
-          frameon=False, prop=dict(size=12))
+ax.legend(loc='lower left', frameon=False, fontsize=12)
 ax.set_xticks(range(len(xlocs)))
 ax.set_xticklabels(xlocs)
-ax.set_ylim(-3.0, 3.0)
+ax.axis([-1.0, 2.1, -3.0, 3.0])
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 fig.tight_layout()
