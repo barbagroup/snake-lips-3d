7808e3b5d07886ad8e26dc3dec79c33f08e8bbb7
mesnardo <mesnardo@gwu.edu>
2022-05-21 14:03:40 -0400

    Refine figure centerline velocity both-Re

--- runs/snake3d/both_lips/scripts/plot_u_centerline_profile_35.py
+++ runs/snake3d/both_lips/scripts/plot_u_centerline_profile_35.py
@@ -50,21 +50,17 @@ for Re in cases.keys():
 pyplot.rc('font', family='serif', size=12)
 
 # Plot profile of the mean streamwise velocity along the centerline.
-fig, ax = pyplot.subplots(figsize=(8.0, 4.0))
-ax.set_xlabel('x / D')
+fig, ax = pyplot.subplots(figsize=(8.0, 3.0))
+ax.set_xlabel('$x / c$')
 ax.set_ylabel(r'$<u> / U_\infty$')
-U_inf, D = 1.0, 1.0
+U_inf, c = 1.0, 1.0
 ax.axhline(0.0, color='gray', linestyle='--')
+ax.plot(*body['1k'], color='black')
 for key, vel_obj in data.items():
-    ax.plot(vel_obj.x / D, vel_obj.values / U_inf, label=cases[key])
-ax.set_xlim(0.0, 4.0)
-inset_ax = fig.add_axes([0.1, 0.6, 0.3, 0.3])
-for label, vel_obj in data.items():
-    inset_ax.plot(*body[label], color='black')
-    inset_ax.axhline(0.0, xmin=0.6, xmax=1.0, color='gray', linestyle='--')
-inset_ax.axis('scaled')
-inset_ax.axis('off')
-ax.legend(frameon=False, loc='center right')
+    ax.plot(vel_obj.x / c, vel_obj.values / U_inf, label=cases[key])
+ax.axis('scaled')
+ax.set_xlim(-0.6, 4.0)
+ax.legend(frameon=False, loc='upper left', fontsize=12)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 fig.tight_layout()
