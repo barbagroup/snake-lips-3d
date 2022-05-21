caabd277d0c9f3d88356c1c9d68570c6be0fc699
mesnardo <mesnardo@gwu.edu>
2022-05-21 12:26:44 -0400

    Refine figure centerline velocity both-2k

--- runs/snake3d/both_lips/scripts/plot_u_centerline_profile_2k.py
+++ runs/snake3d/both_lips/scripts/plot_u_centerline_profile_2k.py
@@ -52,7 +52,7 @@ pyplot.rc('font', family='serif', size=12)
 
 # Plot profile of the mean streamwise velocity along the centerline.
 fig, ax = pyplot.subplots(figsize=(8.0, 4.0))
-ax.set_xlabel('x / D')
+ax.set_xlabel('$x / c$')
 ax.set_ylabel(r'$<u> / U_\infty$')
 U_inf, D = 1.0, 1.0
 ax.axhline(0.0, color='gray', linestyle='--')
@@ -65,7 +65,7 @@ for label, vel_obj in data.items():
     inset_ax.axhline(0.0, xmin=0.6, xmax=1.0, color='gray', linestyle='--')
 inset_ax.axis('scaled')
 inset_ax.axis('off')
-ax.legend(frameon=False, loc='center right')
+ax.legend(frameon=False, loc='center right', fontsize=12)
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 fig.tight_layout()
